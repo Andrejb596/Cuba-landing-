94c89e153e7e22abcc216e233e932d55c8d09fde
Andrejb596 <andrejb596@gmail.com>
2022-09-19 21:32:31 +0300

    'header'/'reason'/half of 'about'

---
+++ script/skript.js
@@ -0,0 +1,20 @@
+const items = document.querySelectorAll('.menu__list-link');
+items.forEach((item) =>{
+    item.addEventListener('click', ()=>{
+        document.querySelector('.menu__list-link.active').classList.remove('active');
+        item.classList.add('active');
+
+    })
+})
+
+$(function(){
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="./images/arrow-left.svg" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./images/arrow-right.svg" alt="right"></button>',
+    });
+    
+})
+
+$('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+});

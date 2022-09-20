61f1ecbc2f0d63a5a6e2db4f137f5ed71315999c
Andrejb596 <andrejb596@gmail.com>
2022-09-20 23:03:08 +0300

    можливо буду править

--- script/skript.js
+++ script/skript.js
@@ -1,20 +1,32 @@
 const items = document.querySelectorAll('.menu__list-link');
-items.forEach((item) =>{
-    item.addEventListener('click', ()=>{
+items.forEach((item) => {
+    item.addEventListener('click', () => {
         document.querySelector('.menu__list-link.active').classList.remove('active');
         item.classList.add('active');
 
     })
 })
 
-$(function(){
+$(function () {
     $('.photo__slider').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="./images/arrow-left.svg" alt="left"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="./images/arrow-right.svg" alt="right"></button>',
+        responsive: [
+            {
+                breakpoint: 500,
+                settings: {
+                    arrows: false,
+                    autoplay: true,
+                    autoplayspeed: 2000,
+                }
+            }
+        ],
     });
-    
+
 })
 
-$('.menu__btn').on('click', function(){
+$('.menu__btn').on('click', function () {
     $('.menu__list').slideToggle();
 });
+
+
